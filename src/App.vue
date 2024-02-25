f4990e8fc6eb73e209a523b9d572ee4469d7756d
SisypheanBoulder <adrian.stack@proton.me>
2024-02-25 21:41:27 +1100

    vue project start

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    {{ user.email }} = {{ fullname }}
+    <strong>likes: </strong> {{ counter }}
+    <button @click="followUser">like</button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      counter: 0,
+      msg: 'Welcome to Your Vue.js App',
+      user: {
+        firstname: 'John',
+        lastname: 'Doe',
+        email: 'johndoe@mail.com',
+        isAdmin: true
+      }
+    }
+  },
+  computed: {
+    fullname() {
+      return `${this.user.firstname} ${this.user.lastname}`
+    }
+  },
+  methods: {
+    followUser() {
+      this.counter++
+    }
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
