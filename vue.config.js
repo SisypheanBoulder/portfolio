b3079b672c395eb1b33b800f3679e2660996cc65
SisypheanBoulder <111966724+SisypheanBoulder@users.noreply.github.com>
2024-02-27 21:53:19 +1100

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: "/portfolio/",
+      publicPath: process.env.NODE_ENV === 'production'
+      ? '/my-project/'
+      : '/'
+  },
   transpileDependencies: true
 })
