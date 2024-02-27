868eadb73e339b6183fed4f91b9b1ae709cffd07
adev <adrian.stack@proton.me>
2024-02-27 22:07:32 +1100

    vue.config update

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  publicPath: "/portfolio/",
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/portfolio/'
+    : '/',
   transpileDependencies: true
-})
+}
